a65a040c992fd7f5597611e2e0cd51993429f75a
masterpieceLyu <elzkdntm@naver.com>
2022-05-25 22:30:06 +0900

    add click animation need contents rayout

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const Main = document.querySelector("#main");
+const HiddenContainer = document.querySelector(".tag-hidden_container");
 let barCode = document.querySelector(".barcode");
 let tag = document.querySelector(".tag");
+let pitAndTitle = document.querySelector(".tag-pitAndTitle");
+let paragraphInMainTag = document.querySelector(".tag-paragraph");
 
 function clickBarCode() {
   Main.classList.toggle("active");
+  HiddenContainer.classList.toggle("active");
   tag.classList.toggle("active");
+  pitAndTitle.classList.toggle("active");
+  paragraphInMainTag.classList.toggle("active");
+  barCode.classList.toggle("active");
 }
 
 barCode.addEventListener("click", clickBarCode);
