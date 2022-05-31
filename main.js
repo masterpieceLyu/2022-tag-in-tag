99276b2971a8a68d1713ddede58a15434c27c1b6
masterpieceLyu <elzkdntm@naver.com>
2022-05-31 18:46:05 +0900

    add contents need scroll method

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const HTML = document.querySelector("html");
 const Main = document.querySelector("#main");
 const HiddenContainer = document.querySelector(".tag-hidden_container");
 let tagSection = document.querySelectorAll(".tag-section");
@@ -25,3 +26,52 @@ function clickBarCode() {
 }
 
 barCode.addEventListener("click", clickBarCode);
+
+// section scroll setup //
+// section scroll setup //
+// section scroll setup //
+addEventListener("scroll", function () {
+  let absoluteTop1 = tagSection[0].getBoundingClientRect().top;
+  if (absoluteTop1.toFixed(0) > 0) {
+    HTML.classList.remove("snap");
+  }
+});
+// section 1
+addEventListener("scroll", function () {
+  let absoluteTop1 = tagSection[0].getBoundingClientRect().top;
+  let absoluteTop2 = tagSection[1].getBoundingClientRect().top;
+
+  if (absoluteTop1.toFixed(0) <= 0 && absoluteTop2.toFixed(0) > 0) {
+    HTML.classList.add("snap");
+    tagSection[1].classList.remove("apear");
+  }
+});
+// section 2
+addEventListener("scroll", function () {
+  let absoluteTop2 = tagSection[1].getBoundingClientRect().top;
+  let absoluteTop3 = tagSection[2].getBoundingClientRect().top;
+
+  if (absoluteTop2.toFixed(0) <= 0 && absoluteTop3.toFixed(0) > 0) {
+    HTML.classList.add("snap");
+    tagSection[1].classList.add("apear");
+  }
+});
+// section 3
+addEventListener("scroll", function () {
+  let absoluteTop3 = tagSection[2].getBoundingClientRect().top;
+  let absoluteTop4 = tagSection[3].getBoundingClientRect().top;
+
+  if (absoluteTop3.toFixed(0) <= 0 && absoluteTop4.toFixed(0) > 0) {
+    HTML.classList.add("snap");
+    tagSection[1].classList.remove("apear");
+  }
+});
+// section 4
+addEventListener("scroll", function () {
+  let absoluteTop3 = tagSection[2].getBoundingClientRect().top;
+  let absoluteTop4 = tagSection[3].getBoundingClientRect().top;
+
+  if (absoluteTop4.toFixed(0) <= 0 && absoluteTop3.toFixed(0) < 0) {
+    tagSection[1].classList.remove("apear");
+  }
+});
