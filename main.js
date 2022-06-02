754fb34ef22400f35bb4f7a75a987e39650b0350
masterpieceLyu <elzkdntm@naver.com>
2022-06-02 20:48:55 +0900

    add animation and neeeeeeed~~ more design and contents

--- main.js
+++ main.js
@@ -32,16 +32,7 @@ barCode.addEventListener("click", clickBarCode);
 // Section Scroll Setup //
 // Section Scroll Setup //
 // Section Scroll Setup //
-addEventListener("scroll", function () {
-  let absoluteTop1 = tagSection[0].getBoundingClientRect().top.toFixed(0);
-  let absoluteTop2 = tagSection[1].getBoundingClientRect().top.toFixed(0);
-  let absoluteTop3 = tagSection[2].getBoundingClientRect().top.toFixed(0);
-  let absoluteTop4 = tagSection[3].getBoundingClientRect().top.toFixed(0);
 
-  if (absoluteTop1 > 0) {
-    HTML.classList.remove("snap");
-  }
-});
 // section 1
 addEventListener("scroll", function () {
   let absoluteTop1 = tagSection[0].getBoundingClientRect().top.toFixed(0);
@@ -49,10 +40,8 @@ addEventListener("scroll", function () {
   let absoluteTop3 = tagSection[2].getBoundingClientRect().top.toFixed(0);
   let absoluteTop4 = tagSection[3].getBoundingClientRect().top.toFixed(0);
 
-  if (absoluteTop1 <= 0 && absoluteTop2 > 0) {
-    tagSection[1].classList.remove("apear");
-    tagSection[2].classList.remove("apear");
-    tagSection[3].classList.remove("apear");
+  if (absoluteTop1 < 0 && absoluteTop2 > 0) {
+    HTML.classList.add("snap");
   }
 });
 // section 2
@@ -62,25 +51,57 @@ addEventListener("scroll", function () {
   let absoluteTop3 = tagSection[2].getBoundingClientRect().top.toFixed(0);
   let absoluteTop4 = tagSection[3].getBoundingClientRect().top.toFixed(0);
 
+  let section2span = document.querySelector(".section2span");
+  let section2contents = document.querySelectorAll(".section2contents");
+  let boxLine = document.querySelectorAll(".box-line");
+
   if (absoluteTop1 <= (absoluteTop2 - absoluteTop1) / 2) {
     HTML.classList.add("snap");
-    tagSection[1].classList.add("apear");
-    tagSection[2].classList.remove("apear");
-    tagSection[3].classList.remove("apear");
+    section2span.style.animation = "slideText 0.8s 0.8s ease-in-out forwards";
+    section2contents[0].style.animation =
+      "slideContents 1.3s 3.1s ease-out forwards";
+    section2contents[1].style.animation =
+      "slideContents 1.3s 3.4s ease-out forwards";
+    section2contents[2].style.animation =
+      "slideContents 1.3s 3.7s ease-out forwards";
+    section2contents[3].style.animation =
+      "slideContents 1.3s 4s ease-out forwards";
+    boxLine[0].style.animation =
+      "box1Apear 0.7s 1.6s cubic-bezier(0.85, 0, 0.15, 1) forwards";
+    boxLine[1].style.animation =
+      "box2Apear 0.7s 2.1s cubic-bezier(0.85, 0, 0.15, 1) forwards";
   }
 });
 // section 3
-addEventListener("scroll", function () {
+addEventListener("scroll", function section3() {
   let absoluteTop1 = tagSection[0].getBoundingClientRect().top.toFixed(0);
   let absoluteTop2 = tagSection[1].getBoundingClientRect().top.toFixed(0);
   let absoluteTop3 = tagSection[2].getBoundingClientRect().top.toFixed(0);
   let absoluteTop4 = tagSection[3].getBoundingClientRect().top.toFixed(0);
 
+  let section3span = document.querySelector(".section3span");
+  let section3contents = document.querySelectorAll(".section3contents");
+
   if (absoluteTop3 <= (absoluteTop2 - absoluteTop1) / 2) {
     HTML.classList.add("snap");
-    tagSection[1].classList.remove("apear");
-    tagSection[2].classList.add("apear");
-    tagSection[3].classList.remove("apear");
+
+    section3span.style.animation = "slideText 0.8s 0.8s ease-in-out forwards";
+    section3contents[0].style.animation =
+      "slideContents 1.3s 1.5s ease-out forwards";
+    section3contents[1].style.animation =
+      "slideContents 1.3s 1.8s ease-out forwards";
+    section3contents[2].style.animation =
+      "slideContents 1.3s 2s ease-out forwards";
+    section3contents[3].style.animation =
+      "slideContents 1.3s 2.2s ease-out forwards";
+    section3contents[4].style.animation =
+      "slideContents 1.3s 2.4s ease-out forwards";
+    section3contents[5].style.animation =
+      "slideContents 1.3s 2.6s ease-out forwards";
+    section3contents[6].style.animation =
+      "slideContents 1.3s 2.8s ease-out forwards";
+    section3contents[7].style.animation =
+      "slideContents 1.3s 3s ease-out forwards";
   }
 });
 // section 4
@@ -90,9 +111,24 @@ addEventListener("scroll", function () {
   let absoluteTop3 = tagSection[2].getBoundingClientRect().top.toFixed(0);
   let absoluteTop4 = tagSection[3].getBoundingClientRect().top.toFixed(0);
 
+  let section4map = document.querySelector(".section4map");
+  let section4copy = document.querySelector(".section4copy");
+
   if (absoluteTop4 <= (absoluteTop4 - absoluteTop3) / 2) {
-    tagSection[1].classList.remove("apear");
-    tagSection[2].classList.remove("apear");
-    tagSection[3].classList.add("apear");
+    section4map.style.animation = "slideContents 1s 0.8s ease-in-out forwards";
+    section4copy.style.animation =
+      "container-apear 1s 0.8s ease-in-out forwards";
   }
 });
+
+const goToTopBtn = document.querySelector(".toTop");
+let rootElement = document.documentElement;
+
+function scrollToTop() {
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+goToTopBtn.addEventListener("click", scrollToTop);
